a9eea0841aff723636c8c1fedbe55a93337d88b2
= <ecesenacs@gmail.com>
2023-10-09 12:00:50 +0300

    hotfix: added hardcoded method

--- src/helpers.py
+++ src/helpers.py
@@ -1,5 +1,7 @@
 import iso3166
+from babel import Locale
 
+countries = {"tr":"tr","us":"en","de":"de"}
 
 def ISO_1366_countries():
     """return ISO_1366 countries """
@@ -7,10 +9,13 @@ def ISO_1366_countries():
 
 
 def is_ISO_3166_country_code(country_code:str):
-    pass
+    return country_code in iso3166.countries
 
-def is_ISO_639_lang_code(lang_code:str):
-    pass
-
-def get_ISO639_code_from_ISO_1366(country_code:str) -> str:
+def get_ISO639_code_from_ISO_1366(country_code:str):
     """return ISO 639-1 code (language code) from country code """
+    # TODO find an automatic way to include all countries
+    # Get the country object based on the ISO 3166 code
+
+    if country_code.lower() in countries:
+        return countries.get(country_code.lower())
+
