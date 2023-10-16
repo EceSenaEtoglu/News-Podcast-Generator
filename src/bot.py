75256ae1636ac4a91bb5ba5058a7dd8e7723b903
= <ecesenacs@gmail.com>
2023-10-16 17:52:42 +0300

    finished: bot functionality

--- src/bot.py
+++ src/bot.py
@@ -4,6 +4,7 @@ from config import *
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 
+## GLOBALS
 START_MESSAGE = "I can get you the top headlines and breaking news for a country (in the country's official language) in an audited format.\n" \
                 "\nHere is what I can do:\n\n" \
                 "1- Get news for a country from the country's sources.\n" \
@@ -23,14 +24,13 @@ INVALID_RESPONSE_ERROR = "I'm not sure I understand your response. To see what I
 
 api = Api(NEWS_API_KEY)
 
-
 # Commands
-async def start_command(update: Update,context):
+async def start_command(update: Update, context):
     """handle the /start command"""
     await update.message.reply_text(START_MESSAGE)
 
 
-async def getnews(update: Update,context):
+async def getnews(update: Update, context):
     """handle the /getnews command"""
     info_message = "To get top headlines and breaking news in an audited format:\n" \
                    "type this without '< >':\n <insert your country code according to ISO 3166-1>'"
@@ -38,7 +38,7 @@ async def getnews(update: Update,context):
     await update.message.reply_text(info_message)
 
 
-async def getnews_category(update: Update,context):
+async def getnews_category(update: Update, context):
     """handle the /getnews_category command"""
 
     info_message = "Here are possible categories:\n" \
@@ -57,15 +57,40 @@ async def getnews_category(update: Update,context):
 
 
 # Logic
-def process_user_message(user_message: str, output_file_name:str, context: ContextTypes.DEFAULT_TYPE) -> str:
-    """Create audio if applicable and return the absolute path.
-    If not applicable returns error message"""
+def get_audio(context, intro: str, operation: str, outputfile_name, country_code: str, category=None):
+    """Create audio based on operation, return the audio path"""
+    if operation == "getnews":
+        articles = api.get_top_headlines(country_code)
+
+        # create audio
+        audio = Audio.from_country_code(articles, country_code, intro, outputfile_name)
+        audio.create_audio()
+
+        return audio.OUTPUT_NAME
+
+    elif operation == "getnews_category":
+
+        articles = api.get_top_headlines(country_code, category)
+
+        # create audio
+        audio = Audio.from_country_code(articles, country_code, intro, outputfile_name)
+        audio.create_audio()
+
+        return audio.OUTPUT_NAME
+
+
+async def process_user_message(user_message: str, update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    """Identify operation based on user_message
+
+    If operation is not valid reply with a warning string
+
+    Else reply with operation string, apply operation and return created path
+    Return: path of the created audio"""
+
+    # audio output file name if audio will be generated
+    output_file_name = f"{update.message.chat_id}.mp3"
 
     response_data = user_message.split(" ")
-
-    # keep track if audio is generated
-    context.user_data["generated_audio"] = False
-
     # user possibly asked for news in a country
     if len(response_data) == 1:
 
@@ -75,24 +100,23 @@ def process_user_message(user_message: str, output_file_name:str, context: Conte
         # if given country code is valid
         if helpers.is_ISO_3166_country_code(country_code):
 
-            articles = api.get_top_headlines(country_code)
+            audio_intro = f"latest news in {helpers.get_country_name(country_code)}"
+            bot_response = f"Retrieving {audio_intro}. Loading..."
+            await update.message.reply_text(bot_response)
 
-            # intro to start the audio with
-            intro = f"Latest news in {helpers.get_country_name(country_code)}"
-
-            # create audio
-            audio = Audio.from_country_code(articles, country_code, intro, output_file_name)
-            audio.create_audio()
-
-            context.user_data["generated_audio"] = True
-            bot_response = audio.get_audio_path()
+            return get_audio(context, audio_intro, "getnews", output_file_name, country_code)
 
         # given country code is not valid
         else:
+
             bot_response = f"Country code {country_code} is not supported. Please check if it is in ISO 3166 format.\n" \
                            f"If it is, please contact admin"
 
+            await update.message.reply_text(bot_response)
+
+
     # user possibly asked for news in a country in a specific category
+
     elif len(response_data) == 2:
 
         # get list of articles
@@ -104,33 +128,32 @@ def process_user_message(user_message: str, output_file_name:str, context: Conte
 
             if category in api.CATEGORIES:
 
-                articles = api.get_top_headlines(country_code, category)
+                audio_intro = f"latest news in {helpers.get_country_name(country_code)} about {category}"
+                bot_response = f"Retrieving {audio_intro}. Loading..."
+                await update.message.reply_text(bot_response)
 
-                # intro to start the audio with
-                intro = f"Latest news in {helpers.get_country_name(country_code)} about {category}"
+                return get_audio(context, audio_intro, "getnews_category", output_file_name, country_code, category)
 
-                # create audio
-                audio = Audio.from_country_code(articles, country_code, intro, output_file_name)
-                audio.create_audio()
-
-                context.user_data["generated_audio"] = True
-                bot_response = audio.get_audio_path()
-
+            # category is not valid
             else:
                 bot_response = f"Category {category} is not supported. To see the supported categories type /getnews_category"
+                await update.message.reply_text(bot_response)
 
         else:
             bot_response = f"Country code {country_code} is not supported. Please check if it is in ISO 3166 format.\n" \
                            f"If it is, please contact admin"
+            await update.message.reply_text(bot_response)
 
+    # unidentified response structure
     else:
         bot_response = INVALID_RESPONSE_ERROR
-
-    return bot_response
+        await update.message.reply_text(bot_response)
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """"Send audio to chat if audio was created else send error message """
+    """Handle user message
+
+    Send audio to chat if audio was created else send error message """
 
     # identify if group or private
     message_type = update.message.chat.type
@@ -138,33 +161,25 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # user response
     user_response = update.message.text
 
-    user_id = update.message.chat.id
-    output_file_name = f"{user_id}.mp3"
-
     if message_type == "group":
 
         if BOT_USERNAME in user_response:
             user_response = user_response.replace(BOT_USERNAME, "")
-            bot_response = process_user_message(user_response, output_file_name, context)
+            audio_path = await process_user_message(user_response, update, context)
 
         else:
             return
     else:
-        bot_response = process_user_message(user_response, output_file_name, context)
+        audio_path = await process_user_message(user_response, update, context)
+
+    if audio_path:
+        try:
+            with open(audio_path, 'rb') as audio:
+                await update.message.reply_audio(audio=audio)
+        except FileNotFoundError as e:
+            print(e)
 
 
-    # if audio is generated
-    # send audio to chat
-    if context.user_data["generated_audio"]:
-        print("sending the audio")
-        audio_file = open(output_file_name, 'rb')
-        await update.message.reply_audio(audio= audio_file)
-        context.user_data["generated_audio"] = False
-
-    # if audio is not generated
-    # send error message
-    else:
-        await update.message.reply_text(bot_response)
 
 
 # Debugger function for the developer
