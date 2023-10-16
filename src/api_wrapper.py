8c788593b40b9303d985aba5526ad4b16bd89f28
= <ecesenacs@gmail.com>
2023-10-16 23:24:56 +0300

    chore: seperated api and article class, closes #8

--- src/api_wrapper.py
+++ src/api_wrapper.py
@@ -9,31 +9,63 @@ class InvalidInputError(Exception):
         self.message = message
         super().__init__(self.message)
 
-
 class Api:
 
     #https://newsapi.org/
 
     CATEGORIES = ["business","entertainment","general","health","science","sports","technology"]
-
-    # countries are all ISO 3166 countrys
+    CONTENT_LENGTH_UPPER_BOUND = 30
 
     def __init__(self, api_key: str):
         self._api_key = api_key
-    @staticmethod
-    def _get_parsed_response(res_data: str):
+
+    def _trim_article_content(self,content):
+        """Trim full article content close to specified upper bound
+
+        Trims the article at stopping by the closest "." to the upperbound
+        """
+
+        if content:
+            # get the approximate words
+            if len(content) >= Api.CONTENT_LENGTH_UPPER_BOUND:
+
+                for i in range(len(content)):
+                    if i >= Api.CONTENT_LENGTH_UPPER_BOUND and content[i] == '.':
+                        break
+
+            # this is not an expected case but still included it because not sure what could be the API's response
+            # if full article content is less than the determined upper bound length
+            else:
+                for i in range(-1, -1 * len(content) - 1, -1):
+                    if content[i] == '.':
+                        # convert i to positive index
+                        i = len(content) + i
+                        break
+
+            return content[:i]  # full content of the article
+
+    def _get_parsed_response(self,res):
         """return list of articles from parsing json data"""
 
-        print(res_data)
+        articles_data = res.json()
 
-        parsed_data = json.loads(res_data)
-        articles_data = parsed_data.get("articles", [])
-
+        articles_data = articles_data.get("articles")
         articles = []
 
         # create list of article objects from article data
         for article_data in articles_data:
-            article = Article(article_data)
+
+            author = article_data["author"]
+            title = article_data["title"]
+            source = article_data["source"]
+            url = article_data["url"]
+            published_at = article_data["publishedAt"]
+
+            # short summary of content
+            description = article_data["description"]
+
+            trimmed_content = self._trim_article_content(article_data["content"])
+            article = Article(author,title,source,published_at,url,description,trimmed_content)
             articles.append(article)
 
         return articles
@@ -57,7 +89,7 @@ class Api:
         if res.status_code != 200:
             raise RuntimeError("Error fetching the headlines")
 
-        return self._get_parsed_response(res.text)
+        return self._get_parsed_response(res)
 
     @dispatch(str)
     def get_top_headlines(self, country_code: str) -> list:
@@ -76,4 +108,4 @@ class Api:
         if res.status_code != 200:
             raise RuntimeError("Error fetching the headlines")
 
-        return self._get_parsed_response(res.text)
+        return self._get_parsed_response(res)
--- src/article.py
+++ src/article.py
@@ -1,50 +1,25 @@
 class Article:
 
-    def __init__(self, article_data: dict):
+    def __init__(self, author:str,title:str,source:str,published_at:str,url:str,description:str,content:str):
 
-        self._content_length_upper_bound = 25
+        self._author = author
+        self._title = title
+        self._source = source
+        self._url = url
+        self._published_at = published_at
 
-        self._author = article_data["author"]
-        self._title = article_data["title"]
-        self._url = article_data["url"]
-        self._source = article_data["source"]
-        self._published_at = article_data["publishedAt"]
+        self._description = description # summary of content
+        self._content = content
 
-        self._description = article_data["description"]  # summary of content
-        self._content = article_data["content"]
+    def _get_content(self) -> str:
+        """Return description (short summary from API) if exits else return content
 
-        # some news data provided by the API gives null description
-        # get description from content(full article) for describing the article
+        If content also does not exist return none"""
+        if self._description:
+            return self._description
 
-        if self._description == None:
-
-            self._content = self._get_content(article_data)
-
-
-        # if description is not none
-        # don't store content
         else:
-            self._content = None
+            return self._content
 
-    def _get_content(self,article_data) -> str:
-        if article_data["content"]:
-
-            if len(article_data["content"]) >= self._content_length_upper_bound:
-
-                # get the approximate words
-                for i in range(len(article_data["content"])):
-                    if i >= self._content_length_upper_bound and article_data["content"][i] == '.':
-                        break
-
-            # this is not an expected case but still included it because not sure what could be the API's response
-            # if full article content is less than the determined upper bound length
-            else:
-                for i in range(-1, -1 * len(article_data["content"]) - 1, -1):
-                    if article_data["content"][i] == '.':
-                        # convert i to positive index
-                        i = len(article_data["content"]) + i
-                        break
-
-            return article_data["content"][:i]  # full content of the article
     def __repr__(self):
         return f"Author:{self._author}, Title:{self._title}, Url:{self._url},Description:{self._description}\n,Content:{self._content},PublishedAt:{self._published_at}"
