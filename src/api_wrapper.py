022d999613b0a1dc0d066870c2b786fd253c952e
= <ecesenacs@gmail.com>
2023-10-17 11:05:29 +0300

    bugfix: raised runtime error when problematic response is turned by api

--- src/api_wrapper.py
+++ src/api_wrapper.py
@@ -1,7 +1,6 @@
 from multipledispatch import dispatch
 from article import Article
 import requests
-import json
 import helpers
 
 class InvalidInputError(Exception):
@@ -19,13 +18,23 @@ class Api:
     def __init__(self, api_key: str):
         self._api_key = api_key
 
-    def _trim_article_content(self,content):
+    @staticmethod
+    def _trim_article_content(content,author,source,title):
         """Trim full article content close to specified upper bound
 
         Trims the article at stopping by the closest "." to the upperbound
+        Raise RuntimeError if can't be trimmed
         """
 
         if content:
+
+            # if article does not contain any "."
+            if "." not in content:
+                message = f"Problem in trimming content \n "\
+                f"Article {title} written by {author} from {source} does not contain any '.'" \
+
+                raise RuntimeError(message)
+
             # get the approximate words
             if len(content) >= Api.CONTENT_LENGTH_UPPER_BOUND:
 
@@ -35,6 +44,7 @@ class Api:
 
             # this is not an expected case but still included it because not sure what could be the API's response
             # if full article content is less than the determined upper bound length
+            # find nearest "." to the end
             else:
                 for i in range(-1, -1 * len(content) - 1, -1):
                     if content[i] == '.':
@@ -42,7 +52,7 @@ class Api:
                         i = len(content) + i
                         break
 
-            return content[:i]  # full content of the article
+            return content[:i+1]  # trimmed content
 
     def _get_parsed_response(self,res):
         """return list of articles from parsing json data"""
@@ -64,7 +74,7 @@ class Api:
             # short summary of content
             description = article_data["description"]
 
-            trimmed_content = self._trim_article_content(article_data["content"])
+            trimmed_content = Api._trim_article_content(article_data["content"],author,source,title)
             article = Article(author,title,source,published_at,url,description,trimmed_content)
             articles.append(article)
 
@@ -87,7 +97,7 @@ class Api:
         res = requests.get(url)
 
         if res.status_code != 200:
-            raise RuntimeError("Error fetching the headlines")
+            raise RuntimeError(f"Error fetching the headlines for {country_code}")
 
         return self._get_parsed_response(res)
 
@@ -106,6 +116,6 @@ class Api:
         res = requests.get(url)
 
         if res.status_code != 200:
-            raise RuntimeError("Error fetching the headlines")
+            raise RuntimeError(f"Error fetching the headlines  for {country_code}")
 
         return self._get_parsed_response(res)
