972b352fe3a08beff14cd447f4aa99c4a8a9d009
= <ecesenacs@gmail.com>
2023-10-20 12:45:12 +0300

    fix: added hardcoded conversion for all countries to languages. API only included 54 countries, with help of a python scripy and chatgpt generated a dictionary for conversion. Closes #6

--- src/helpers.py
+++ src/helpers.py
@@ -1,6 +1,11 @@
 import iso3166
 
-countries = {"tr": "tr", "us": "en", "de": "de","es":"es","fr":"fr"}
+# keys: countries supported by the API values: their official lang in ISO 639-1 format
+country_to_lang= {'ae': 'ar', 'ar': 'es', 'at': 'de', 'au': 'en', 'be': 'nl', 'bg': 'bg', 'br': 'pt', 'ca': 'en', 'ch': 'de', 'cn': 'zh', 'co': 'es', 'cu': 'es',
+'cz':'cs','de':'de','eg':'ar','fr':'fr','gb':'en','gr':'el','hk':'zh','hu':'hu','id':'id','ie':'en','il':'he','in':'hi',
+'it':'it','jp':'ja','kr':'ko','lt':'lt','lv':'lv','ma':'ar','mx':'es','my':'ms','ng':'en','nl':'nl','no':'no','nz':'en',
+'ph':'en','pl':'pl','pt':'pt','ro':'ro','rs':'sr','ru':'ru','sa':'ar','se':'sv','sg':'en','si':'sl','sk':'sk','th':'th',
+'tr':'tr','tw':'zh','ua':'uk','us':'en','ve':'es','za':'af'}
 
 def get_country_name(country_code: str):
     return iso3166.countries[country_code].name
@@ -12,7 +17,6 @@ def is_ALPHA2_ISO_3166_country_code(country_code: str):
 
 def get_ISO639_code_from_ISO_1366(country_code: str):
     """return ISO 639-1 code (language code) from country code """
-    # TODO find an automatic way to include all countries
-
-    if country_code.lower() in countries:
-        return countries.get(country_code.lower())
+    # couldn't find conversion via a package, used a dict
+    if country_code.lower() in country_to_lang:
+        return country_to_lang.get(country_code.lower())
